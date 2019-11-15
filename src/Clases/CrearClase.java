bef51cbc9b30703c12017f7257bb433161fd9e96
cristhian <cristhian.carrero@uptc.edu.co>
2019-11-15 15:51:52 -0500

    PruebaUno1

---
+++ src/Clases/CrearClase.java
@@ -0,0 +1,15 @@
+package Clases;
+
+public class CrearClase {
+
+	public CrearClase() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+
+	}
+
+}
